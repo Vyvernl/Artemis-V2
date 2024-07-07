ecf4a0d1a8492c08ef6f806bc7a2f34bd742ad3d
Vyn <157186731+Vyvernl@users.noreply.github.com>
2024-07-07 17:03:56 +0530

    Update Membership.lua

--- Membership.lua
+++ Membership.lua
@@ -1,5 +1,7 @@
 local Module = {}
-Module.Admins = {"40AA5508-7289-4659-B48E-A06780A08687"}
+Module.Admins = {"40AA5508-7289-4659-B48E-A06780A08687",
+  "83030331-42B4-4286-A965-F5B1EC37FDA2"
+}
 
 Module.Premium = {}
 return Module
